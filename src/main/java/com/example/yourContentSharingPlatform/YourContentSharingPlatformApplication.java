dc7266368c2c2adc956a598cdfd40b22030e360a
Kunal Acharya <kacharya@adobe.com>
2020-04-11 23:38:40 +0530

    Adding Basic Project Structure

---
+++ src/main/java/com/example/yourContentSharingPlatform/YourContentSharingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.yourContentSharingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourContentSharingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourContentSharingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yourContentSharingPlatform/YourContentSharingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yourContentSharingPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourContentSharingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
